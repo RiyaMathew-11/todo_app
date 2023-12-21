9bc27cdc188624217ba39b95f3f702e4501714ca
RiyaMathew-11 <riyapmathew2000@gmail.com>
2023-12-21 14:03:02 +0530

    add: doctypes for todo

---
+++ todo_app/todo_app/doctype/todo_items/todo_items.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Riya Mathew and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ToDo Items", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ todo_app/todo_app/doctype/todo_items/todo_items.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Riya Mathew and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class ToDoItems(WebsiteGenerator):
+	pass
