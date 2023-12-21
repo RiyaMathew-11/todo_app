af8c37a0ac80608f547adeee61a7c9f71067b23f
RiyaMathew-11 <riyapmathew2000@gmail.com>
2023-12-21 14:40:23 +0530

    add: import statements for frappe

--- todo_app/todo_app/doctype/todo_items/todo_items.py
+++ todo_app/todo_app/doctype/todo_items/todo_items.py
@@ -1,9 +1,8 @@
 # Copyright (c) 2023, Riya Mathew and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class ToDoItems(WebsiteGenerator):
-	pass
+    pass
